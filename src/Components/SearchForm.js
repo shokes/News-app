ba8b019cbd712286f0eb381548d4955a9ead075a
shokes <oyatioshoke@yahoo.com>
2022-05-01 20:17:54 +0100

    focus to search input

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,7 +1,13 @@
 import { useGlobalContext } from '../context';
-
+import { useEffect, useRef } from 'react';
 const SearchForm = function () {
   const { search, handleSearch } = useGlobalContext();
+  const searchValue = useRef('');
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
   return (
     <div className='container'>
       <input
@@ -9,6 +15,7 @@ const SearchForm = function () {
         placeholder='search...'
         className='input'
         value={search}
+        ref={searchValue}
         onChange={(e) => handleSearch(e.target.value)}
       />
     </div>
